172fa06eaf943505982a7454ed67b52406b7564e
developer <agraharirehan57@gmail.com>
2024-09-15 17:20:31 +0545

    video

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-storage.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAIgqX-mmVGRe67fhOaZixUUWJEFG5EPyc",
-    authDomain: "studies-972a4.firebaseapp.com",
-    projectId: "studies-972a4",
-    storageBucket: "studies-972a4.appspot.com",
-    messagingSenderId: "588930545350",
-    appId: "1:588930545350:web:5ed62bdd8667362a2a89c7"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIgqX-mmVGRe67fhOaZixUUWJEFG5EPyc",
+  authDomain: "studies-972a4.firebaseapp.com",
+  projectId: "studies-972a4",
+  storageBucket: "studies-972a4.appspot.com",
+  messagingSenderId: "588930545350",
+  appId: "1:588930545350:web:5ed62bdd8667362a2a89c7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+
+
+
+
+
+
+
+
 
 
 
@@ -22,20 +34,29 @@
 
 
 let btn = document.querySelector(".uploadbtn");
+let input = document.querySelector("#fileInput")
 btn.addEventListener("click", ()=>{
-   
+  input.click();
 })
 
 //Animation
 
 const scrollRevealOption={
     distance: '50px',
-    origin: 'bottom',
-    duration: 1700
+    origin: 'top',
+    duration: 1400,
+    // easing: 'cubic-bezier(0.50, -0.45, 0.20, 1.45)',
+    opacity: 0,
+    scale: 0.9,
+    reset: true,
+    mobile: true,
+    // rotate: { x: 10, y: 0, z: 10 },
+    viewFactor: 0.2,
+
 }
 const scrollReveal={
   distance : '50px',
-  origin : 'left',
+  origin : 'bottom',
   duration : 2000
 
 }
@@ -47,6 +68,18 @@ ScrollReveal().reveal('.uploadbtn', scrollRevealOption);
 ScrollReveal().reveal('.topic', scrollReveal);
 ScrollReveal().reveal('.maths', scrollReveal);
 
+const video = document.getElementById('video');
 
+// Promise.all([
+//   faceapi.nets.tnyFaceDetector.loadFromUrl('')
+// ])
 
+function startVideo() {
+  navigator.getUserMedia(
+    {video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
 
+startVideo();
